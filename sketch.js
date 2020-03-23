2cc7adf7994131df8af17920e66c420417bb9410
aldacomadison <32907699+aldacomadison@users.noreply.github.com>
2020-03-22 18:06:41 -0700

    edits 3/22

--- sketch.js
+++ sketch.js
@@ -1,7 +1,65 @@
+var ship;
+var asteroids = [];
+var lasers = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
+  for (var i = 0; i < 10; i++){
+  asteroids.push(new Asteroid());
+}
 }
 
 function draw() {
-  background(255);
+  background(13,26,129);
+
+  for (var i = 0; i < asteroids.length; i++){
+    if (ship.hits(asteroids[i])){
+      console.log('ooops');
+    }
+    asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
+  }
+
+  for (var i = lasers.length-1; i >= 0; i--){
+    lasers[i].render();
+    lasers[i].update();
+    for (var j = asteroids.length-1; j >= 0; j--){
+      if (lasers[i].hits(asteroids[j])){
+        if (asteroids[j].r > 10) {
+    fill(0);
+    var newAsteroids = asteroids[j].breakup();
+    asteroids = asteroids.concat(newAsteroids);
+  }
+    asteroids.splice(j, 1);
+    lasers.splice(i,1);
+    break;
+  }
+  }
+
+}
+
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+
+}
+
+function keyReleased() {
+  ship.setRotation(0);
+  ship.boosting(false);
+}
+
+function keyPressed() {
+  if (key == ' '){
+    lasers.push(new Laser(ship.pos, ship.heading));
+  } else if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);
+  }
 }
